8d8da38ed8c39636e9ece9f095e1d1bec041096a
Olukayode Ogunnowo <olukayode.ogunnowo@yahoo.com>
2024-10-12 09:19:39 +0100

    refactor: updated code based

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -5,7 +5,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
-import Link from "next/Link";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
@@ -23,7 +23,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log(pathname);
 
   return (
     <>
